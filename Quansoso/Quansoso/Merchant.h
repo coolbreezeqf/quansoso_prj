f98261416d0670642db1d624e541127866b218fd
coolbreezeqf <qf>
2014-09-18 12:54:15 +0800

    Model Change

--- Quansoso/Quansoso/Merchant.h
+++ Quansoso/Quansoso/Merchant.h
@@ -11,30 +11,30 @@
 
 @interface Merchant : NSObject <NSCoding, NSCopying>
 
+@property (nonatomic, strong) NSString *merchantIdentifier; //id
 @property (nonatomic, strong) NSString *merchantDescription;
-@property (nonatomic, strong) NSString *status;
-@property (nonatomic, strong) NSString *websiteUrl;
-@property (nonatomic, assign) id tagExpression;
-@property (nonatomic, assign) id brand;
-@property (nonatomic, strong) NSString *shopScore;
-@property (nonatomic, strong) NSString *externalLevel;
-@property (nonatomic, strong) NSString *externalShopId;
-@property (nonatomic, assign) id keywords;
-@property (nonatomic, assign) id memo;
 @property (nonatomic, strong) NSString *name;
-@property (nonatomic, strong) NSString *gmtModified;
+@property (nonatomic, strong) NSString *picUrl;
+@property (nonatomic, strong) NSString *externalId;
+@property (nonatomic, strong) NSString *externalShopId;
 @property (nonatomic, strong) NSString *type;
-@property (nonatomic, strong) NSString *merchantIdentifier;
-@property (nonatomic, assign) id saleVolume;
+@property (nonatomic, strong) NSString *externalCid;
+@property (nonatomic, strong) NSString *externalCategory;
+@property (nonatomic, strong) NSString *externalLevel;
+@property (nonatomic, strong) NSString *brand;
+@property (nonatomic, strong) NSString *websiteUrl;
+@property (nonatomic, strong) NSString *keywords;
+@property (nonatomic, strong) NSString *status;
+@property (nonatomic, strong) NSString *tagExpression;
+@property (nonatomic, strong) NSString *shopScore;
+@property (nonatomic, strong) NSString *memo;
+@property (nonatomic, strong) NSString *gmtModified;
+@property (nonatomic, strong) NSString *saleVolume;
 @property (nonatomic, strong) NSString *gmtCreated;
 @property (nonatomic, strong) NSString *grade;
 @property (nonatomic, strong) NSString *isEnter;
-@property (nonatomic, strong) NSString *externalId;
 @property (nonatomic, strong) NSString *ekpCategory;
-@property (nonatomic, strong) NSString *picUrl;
-@property (nonatomic, assign) id sellerId;
-@property (nonatomic, assign) id externalCategory;
-@property (nonatomic, strong) NSString *externalCid;
+@property (nonatomic, strong) NSString *sellerId;
 
 + (instancetype)modelObjectWithDictionary:(NSDictionary *)dict;
 - (instancetype)initWithDictionary:(NSDictionary *)dict;
