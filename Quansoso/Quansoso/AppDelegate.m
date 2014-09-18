8cd10a66ebb9651004f6ce85a7fb5a8b3ab7d800
锦超 <liuchao.lc@taobao.com>
2014-09-18 15:00:45 +0800

    dfsfs

--- Quansoso/Quansoso/AppDelegate.m
+++ Quansoso/Quansoso/AppDelegate.m
@@ -20,6 +20,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor blackColor];
+    
     [self initTBSDK];
     [self.window makeKeyAndVisible];
     return YES;
