2b79aa3a1feec85579b4fde1c5b7186e6f417c33
coolbreezeqf <qf>
2014-12-29 17:02:57 +0800

    修改领卡url

--- Quansoso/Quansoso/QSCardDetailsNetManager.m
+++ Quansoso/Quansoso/QSCardDetailsNetManager.m
@@ -11,7 +11,7 @@
 #import <CommonCrypto/CommonDigest.h>
 #define kURLMerchant @"http://quansoso.uz.taobao.com/d/cr/rest"
 #define kURLItems @"http://repository.ekupeng.com/cr/rest?service=ekupeng_item_relate&fields=num_iid,title,nick,pic_url,price,click_url"
-#define kURLLing @"http://repository.ekupeng.com/cr/rest?service=add_exchange&"
+#define kURLLing @"http://quansoso.uz.taobao.com/view/app/add_exchange.php?service=add_exchange&"
 @implementation QSCardDetailsNetManager
 - (void)getCardUseCouponId:(NSString *)couponId andNick:(NSString *)nick success:(void (^)(NSString* describe))succ failure:(void (^)(NSString* describe))failure{
 //	NSDictionary *dic = @{@"nick":nick,@"couponId":couponId,@"service":@"exchange"};
@@ -39,11 +39,14 @@
 		NSString *resultStr = successDict[@"describe"];
 		if(result){
 			succ(resultStr);
+			MLOG(@"调用成功");
 		}else{
 			failure(resultStr);
+			MLOG(@"调用失败");
 		}
 	} failure:^(NSDictionary *failDict, NSError *error) {
 		failure(@"error");
+		MLOG(@"调用失败");
 	}];
 }
 
